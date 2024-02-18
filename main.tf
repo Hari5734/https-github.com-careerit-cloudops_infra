55e2b19c3e63b8bc169bcb934006cb8db836f83e
K.HARVIJAY <k.harivijay@gmail.com>
2024-02-18 20:09:28 +0530

    Adding vnet

--- main.tf
+++ main.tf
@@ -20,3 +20,11 @@ resource "azurerm_resource_group" "rg" {
     location = "eastus"
   
 }
+
+resource "azurerm_virtual_network" "vnet" {
+    name = "tcs-vnet"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    address_space = [ "10.0.0.0/16" ]
+  
+}
